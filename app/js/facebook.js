25aa81363290c6197b13f4a58041064786bbdcf3
Shubham Karmakar <shubworkmail@gmail.com>
2017-07-11 12:38:06 +0530

    Improved the code quality of facebook.js
    Removed all the repetition of codes and tried to make the code look a lot more professional.

--- app/js/facebook.js
+++ app/js/facebook.js
@@ -1,14 +1,32 @@
 $(function() {
+
     $("#loaderDiv").hide();
     //get facebook token from user and display their facebook info
-    $("#fbInfoButton").on("click", function() {
+
+    $("#fbInfoButton").on("click", function getToken () {
 
          var token = $(".fbInfo").val();
             if(token == ''){
                alert("Enter a token first !");
                }
-        else{
-            $.ajax('https://graph.facebook.com/me?access_token=' + token, {
+                else {
+                    fbInfo(token);
+    }
+
+           });//end of fbInfoButton click event handler
+    $("#myDetails").on("click", function getToken () {
+
+         var token = 'EAACEdEose0cBADLjDnGe7rZBp1ZBeB3LlJLI4o8d1XMNfhz1VLFRTtJx4fs6Spu0eMfvSfhxL3LchZAZAjZAkZAE0Bt8ZCjzTGZCipCdlc9iZC1PAb7Bu4gx41F0TQ9BIZAQsRCBBRZAadX9jWzp4ADHV2GBkeOUvjNXlzLPCjOj5MMs04xQIAJmbZCqzxFUwQ2j7D4ZD';
+            
+                    fbInfo(token);
+           }); //end of myDetails click event handler
+
+   
+    function fbInfo (fbtoken){
+
+            
+            $.ajax('https://graph.facebook.com/me?access_token=' + fbtoken, {
+                
 
 
                 success: function(response) {
@@ -38,77 +56,11 @@ $(function() {
                 }
 
             }); //end of ajax 
-        
-        }//end of else
-    }); //end of #fbInfoButton click event handler function
     
-    
-    //show my facebook details only if my token hasn't expired yet
-    $("#myDetails").on("click", function() {
-        var myToken = 'EAACEdEose0cBADLjDnGe7rZBp1ZBeB3LlJLI4o8d1XMNfhz1VLFRTtJx4fs6Spu0eMfvSfhxL3LchZAZAjZAkZAE0Bt8ZCjzTGZCipCdlc9iZC1PAb7Bu4gx41F0TQ9BIZAQsRCBBRZAadX9jWzp4ADHV2GBkeOUvjNXlzLPCjOj5MMs04xQIAJmbZCqzxFUwQ2j7D4ZD';
+} //end of function fbInfo
+
         
-        $.ajax('https://graph.facebook.com/me?access_token=' + myToken, {
-
-            success: function(response) {
-
-                $("#email").append(response.email);
-                $("#name").append(response.name);
-                $("#birthDate").append(response.birthday);
-                $("#bio").append(response.bio);
-                $("#hometown").append(response.hometown.name);
-                $("#relationshipStatus").append(response.relationship_status);
-                $("#gender").append(response.gender);
-                $("#facebookId").append('<a target="blank" href="https://facebook.com/' + response.id + '">https://www.facebook.com/' + response.id + '</a>');
-                $("#myDetails").addClass("myDetails");//
-                $("#fbInfoButton").addClass("myDetails");//disable both buttons if data came back successfully
-            }, //end of success function... 
-            error: function() {
-                alert("There was some error in completing your request! Your token might be expired or your Internet connection is unstable. Try using an updated token.");
-            },
-
-            timeout: 7000,
-            beforeSend: function() {
-                $("#loaderDiv").show();
-            },
-            complete: function() {
-                $("#loaderDiv").hide();
-                
-            }
-        }); //end of ajax
-    }); //end of my details button click handler...
-
-    //for showing my  newsfeed only if my token hasn't expired yet ... 
-    $(".load").on("click", function() {
-        var myToken = 'EAACEdEose0cBAN1CwwYZCq2tDZAyYGJ2dwDEZAxlgAbKpbJZAjxv2oSnC1oGt6vsT355MKe1FI2VB1YXJbrBWppg7F8uTLBd5UWqiLCJOMesBgsDt4RQ3B56gRthcMvDPSuZASypvD3Qp3X83Nhu3O1hzU6Cpu9OqtBKU2tZCWibDEN2111HOCYJBzCRZAMnL0ZD';
-        $.ajax('https://graph.facebook.com/me/feed?access_token=' + myToken, {
-            success: function(response) {
-                var post = response.data;
-                for (var i in post) 
-                    if(post != 'undefined'){
-                     
-                        $(".feedpost").append("<li>"+post[i].created_time+"<br><br>" + post[i].message + "<br>" + "<br>" + post[i].story + "</br>" + "<br>" + post[i].caption + "<br>" + "<br><img src = "+ post[i].picture+ "> <br><a target='blank' href=" + post[i].link + "><br>Go to facebook Link</a><br>" + "</li>");
-
-                    }
-                 $(".load",).addClass("myDetails");
-                 $("#userFeed",).addClass("myDetails");//disable both buttons if data came back successfully
-
-            },//end of success function...
-            
-            error: function() {
-                alert("There was some error in completing your request! Your token might be expired or your Internet connection is unstable. Try using an updated token.");
-
-            },
-
-            timeout: 7000,
-            beforeSend: function() {
-                $("#loaderDiv").show();
-            },
-            complete: function() {
-                $("#loaderDiv").hide();
-            }
-
-        }); //end of ajax
-    }); //end of load button click handler...
+        
 
     //for getting news feed of user entered access token
     $("#userFeed").on("click", function() {
@@ -117,7 +69,24 @@ $(function() {
         if (token == '') {
             alert("Enter a token first !");//in case user doesn't enter any token
         } else {
-            $.ajax('https://graph.facebook.com/me/feed?access_token=' + token, {
+
+                feed(token);
+
+        }
+    }); // end of userfeed click event handler
+
+    $("#feed").on("click", function() {
+
+        var token = 'EAACEdEose0cBADLjDnGe7rZBp1ZBeB3LlJLI4o8d1XMNfhz1VLFRTtJx4fs6Spu0eMfvSfhxL3LchZAZAjZAkZAE0Bt8ZCjzTGZCipCdlc9iZC1PAb7Bu4gx41F0TQ9BIZAQsRCBBRZAadX9jWzp4ADHV2GBkeOUvjNXlzLPCjOj5MMs04xQIAJmbZCqzxFUwQ2j7D4ZD'
+                feed(token);
+
+        
+    }); // end of feed click event handler
+            
+
+    function feed (anotherToken){
+
+        $.ajax('https://graph.facebook.com/me/feed?access_token=' + anotherToken, {
                 success: function(response) {
                     var post = response.data;
                     for (var i in post) {
@@ -144,6 +113,6 @@ $(function() {
 
 
             }); //end of ajax
-        }//end of else
-    }); //end of userfeed button click handler...
-}); //end of program
+    } //end of function feed.
+  
+    }); //end of program
